e1d019b871df197193de2e91e4ff307085ffbebb
Cass Hsu <cass.hsu@gmail.com>
2021-01-02 14:30:00 +0800

    init 0001 approach 01

---
+++ 0001/0001-01.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val m = mutableMapOf<Int, Int>()
+        val res = mutableListOf<Int>()
+        
+        for (i in 0..nums.size-1) {
+            if (m.getOrDefault(nums[i], -1) >= 0) {
+                res.add(m.getOrDefault(nums[i], -1))
+                res.add(i)
+            }
+            m.put(target - nums[i], i)
+        }
+        return res.toIntArray()
+    }
+}
