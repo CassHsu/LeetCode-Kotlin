a9039dac9edcf27387d88f712029f55ca78abbff
CassHsu <cass.hsu@gmail.com>
2024-10-04 22:49:06 +0800

    Create 0111-01.kt

---
+++ 0111/0111-01.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun minDepth(root: TreeNode?): Int {
+        if (root == null) return 0
+        if (root.left == null && root.right == null) return 1
+
+        var minDepth = Int.MAX_VALUE
+        if (root.left != null) {
+            minDepth = Math.min(minDepth, minDepth(root.left))
+        }
+        
+        if (root.right != null) {
+            minDepth = Math.min(minDepth, minDepth(root.right))
+        }
+
+        return minDepth + 1
+    }
+}
